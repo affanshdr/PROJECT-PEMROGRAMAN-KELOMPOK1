1148c1965c41a8f8bf140f8a28a720ad9f790eb2
Rezza Ramadhana <167224813+Lingzhi09@users.noreply.github.com>
2024-05-12 18:41:23 +0700

    Update

--- Fungsi_tambahan.c
+++ Fungsi_tambahan.c
@@ -1,13 +1,5 @@
 #include "Header.h"
 
-void Header(){
-    printf("\n");
-    printf("+--------------------------------------------------------+\n");
-    printf("| \t\t Selamat Datang Di Program \t\t |\n");
-    printf("| \t\t\t Perpustakaan \t\t\t |\n");
-    printf("+--------------------------------------------------------+\n");
-}
-
 void Clear_System(){
     #ifdef _WIN32
         system("cls");
@@ -16,14 +8,184 @@ void Clear_System(){
     #endif
 }
 
+void Header(){
+    printf("\n");
+    printf("+--------------------------------------------------------+\n");
+    printf("| \t\t Selamat Datang Di Program \t\t |\n");
+    printf("| \t\t\t Perpustakaan \t\t\t |\n");
+    printf("+--------------------------------------------------------+\n");
+}
+
+void Header2(){
+    printf("\t +---------------------------------+\n");
+    printf("\t | \t Buku Yang Tersedia \t |\n");
+    printf("\t +---------------------------------+\n\n");
+}
+
+void Header3(){
+    printf("\t +---------------------------------+\n");
+    printf("\t | \t Buku Yang Dipinjam \t |\n");
+    printf("\t +---------------------------------+\n\n");
+}
+
 void Header_Admin(){
     printf("+----------------------------------------+\n");
     printf("| \t\t Login Admin \t\t |\n");
     printf("+----------------------------------------+\n");
 }
 
+void Sign_Up(){
+    printf("\t +---------------------------------+\n");
+    printf("\t | \t\t Sign Up \t\t |\n");
+    printf("\t +---------------------------------+\n\n");
+}
+
+void Log_In(){
+    printf("\t +---------------------------------+\n");
+    printf("\t | \t\t Login \t\t |\n");
+    printf("\t +---------------------------------+\n\n");
+}
+
 void Header_User(){
     printf("+----------------------------------------+\n");
     printf("| \t\t Login User \t\t |\n");
     printf("+----------------------------------------+\n");
+}
+
+void SignUpAdmin() {
+    
+    FILE *file = fopen("Data_Admin.txt", "w"); // Menambahkan data ke file
+
+    if (file == NULL) {
+        printf("Error: Gagal membuka file.\n");
+        return;
+    }
+
+    User newUser;
+
+    printf("\n  Masukkan username: ");
+    fgets(newUser.username, 100, stdin);
+    strtok(newUser.username, "\n"); // Menghapus karakter newline dari input username
+
+    printf("  Masukkan password: ");
+    fgets(newUser.password, 100, stdin);
+    strtok(newUser.password, "\n"); // Menghapus karakter newline dari input password
+
+    fprintf(file, "%s\n%s\n", newUser.username, newUser.password);
+    printf("  Sign-up berhasil!\n");
+
+    fclose(file);
+}
+
+void LoginAdmin(int *Selesai2, char *username) {
+    char inputUsername[100];
+    char inputPassword[100];
+
+    printf("\n  Masukkan username: ");
+    fgets(inputUsername, 100, stdin);
+    strtok(inputUsername, "\n"); // Menghapus karakter newline dari input username
+
+    printf("  Masukkan password: ");
+    fgets(inputPassword, 100, stdin);
+    strtok(inputPassword, "\n"); // Menghapus karakter newline dari input password
+
+    FILE *file = fopen("Data_Admin.txt", "r"); // Membuka file untuk membaca data
+
+    if (file == NULL) {
+        printf("Error: Gagal membuka file.\n");
+        return;
+    }
+
+    User user;
+    char tempUsername[100];
+    char tempPassword[100];
+    int found = 0;
+
+    // Memeriksa setiap baris dalam file
+    while (fgets(tempUsername, 100, file) != NULL) {
+        fgets(tempPassword, 100, file); // Membaca password dari baris berikutnya
+        strtok(tempUsername, "\n"); // Menghapus karakter newline dari input username
+        strtok(tempPassword, "\n"); // Menghapus karakter newline dari input password
+
+        // Memeriksa apakah username dan password cocok
+        if (strcmp(tempUsername, inputUsername) == 0 && strcmp(tempPassword, inputPassword) == 0) {
+            printf("  Login berhasil!\n");
+            (*Selesai2) = 1;
+            strcpy(username, inputUsername); // Menyimpan username yang berhasil login
+            fclose(file);
+            return;
+        }
+    }
+
+    printf("  Username atau password salah.\n");
+    fclose(file);
+}
+
+void SignUp() {
+
+    FILE *file = fopen("Data_User.txt", "w"); // Menambahkan data ke file
+
+    if (file == NULL) {
+        printf("Error: Gagal membuka file.\n");
+        return;
+    }
+
+    User newUser;
+
+    printf("\n  Masukkan username: ");
+    fgets(newUser.username, 100, stdin);
+    strtok(newUser.username, "\n"); // Menghapus karakter newline dari input username
+
+    printf("  Masukkan password: ");
+    fgets(newUser.password, 100, stdin);
+    strtok(newUser.password, "\n"); // Menghapus karakter newline dari input password
+
+    fprintf(file, "%s\n%s\n", newUser.username, newUser.password);
+    printf("  Sign-up berhasil!\n");
+
+    fclose(file);
+}
+
+void Login(int *Selesai2, char *username) {
+    char inputUsername[100];
+    char inputPassword[100];
+
+    printf("\n  Masukkan username: ");
+    fgets(inputUsername, 100, stdin);
+    strtok(inputUsername, "\n"); // Menghapus karakter newline dari input username
+
+    printf("  Masukkan password: ");
+    fgets(inputPassword, 100, stdin);
+    strtok(inputPassword, "\n"); // Menghapus karakter newline dari input password
+
+    FILE *file = fopen("Data_User.txt", "r"); // Membuka file untuk membaca data
+
+    if (file == NULL) {
+        printf("Error: Gagal membuka file.\n");
+        return;
+    }
+
+    User user;
+    char tempUsername[100];
+    char tempPassword[100];
+    int found = 0;
+
+    // Memeriksa setiap baris dalam file
+    while (fgets(tempUsername, 100, file) != NULL) {
+        fgets(tempPassword, 100, file); // Membaca password dari baris berikutnya
+        strtok(tempUsername, "\n"); // Menghapus karakter newline dari input username
+        strtok(tempPassword, "\n"); // Menghapus karakter newline dari input password
+
+        // Memeriksa apakah username dan password cocok
+        if (strcmp(tempUsername, inputUsername) == 0 && strcmp(tempPassword, inputPassword) == 0) {
+            printf("  Login berhasil!\n");
+            (*Selesai2) = 1;
+            strcpy(username, inputUsername); // Menyimpan username yang berhasil login
+            fclose(file);
+            return;
+        }
+    }
+
+    printf("  Username atau password salah.\n");
+    fclose(file);
 }
--- Header.h
+++ Header.h
@@ -17,15 +17,35 @@ typedef struct
     char password[Max_char];
 } User;
 
+typedef struct 
+{
+    char Judul[Max_char];
+    int Jumlah;
+} Borrow;
 
-void Header();
 void Clear_System();
+void Header();
+void Header2();
+void Header3();
 void Header_Admin();
+void Sign_Up();
+void Log_In();
 void Header_User();
+void SignUpAdmin();
+void LoginAdmin(int *, char *);
 void SignUp();
-void Login(int);
+void Login();
 void List_Buku();
 void Display_List_Buku(int, int, int, Perpus[]);
 void Create_Buku();
-void Delete_Buku();
 void Update_Buku();
+void hapusBuku(Perpus[], int *, int);
+void Tulis_Buku(Perpus[], int);
+void Pinjam_Buku(Perpus[], int, Borrow[], int *);
+void Dipinjam(Borrow[], int , char[]);
+void Read_Peminjaman(const char *);
+void Kembalikan_Buku(Perpus[], int, Borrow[], int *, char[]);
+int Baca_Buku(Perpus[]);
+int Baca_Buku_H(Perpus[]);
+int Baca_Dipinjam_H(Borrow[], char[]);
+void Tulis_Dipinjam_H(Borrow[], int, char[]);
