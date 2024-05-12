0d503f7bc8384217fc77d7bde51855640545d2e0
Rezza Ramadhana <167224813+Lingzhi09@users.noreply.github.com>
2024-05-12 11:13:35 +0700

    Update

--- Header.h
+++ Header.h
@@ -2,18 +2,19 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#define Max_char 50
 
 typedef struct
 {
     unsigned int Id_buku;
-    char Judul[100], Penulis[100], Penerbit[100];
+    char Judul[Max_char], Penulis[Max_char], Penerbit[Max_char];
     unsigned int Jumlah_halaman, Tahun_terbit, Jumlah_tersedia;
 } Perpus;
 
 typedef struct 
 {
-    char username[100];
-    char password[100];
+    char username[Max_char];
+    char password[Max_char];
 } User;
 
 
@@ -24,6 +25,7 @@ void Header_User();
 void SignUp();
 void Login(int);
 void List_Buku();
+void Display_List_Buku(int, int, int, Perpus[]);
 void Create_Buku();
 void Delete_Buku();
 void Update_Buku();
--- List_Buku.c
+++ List_Buku.c
@@ -1,36 +1,96 @@
 #include "Header.h"
 
+void Display_List_Buku(int page, int data_per_page, int data, Perpus buku[]){
+    int stock = 0;
+    for(int i = page * data_per_page; i < (page + 1) * data_per_page; i++){
+        if(i == 0){
+            printf("+----------------------------------------------------------------------------------------------------------------------------------------+\n");
+            printf("| Id_buku | \t\t Judul Buku \t\t | \t Penulis \t | \t Penerbit \t | Jumlah_halaman | Tahun_terbit | stock |\n");
+            printf("+----------------------------------------------------------------------------------------------------------------------------------------+\n");
+        }
+        printf("| %-7u | %-36s | %-21s | %-21s | %-14u | %-12u | %-5u |\n", buku[i].Id_buku, buku[i].Judul, 
+                buku[i].Penulis, buku[i].Penerbit, buku[i].Jumlah_halaman, buku[i].Tahun_terbit, buku[i].Jumlah_tersedia);
+        printf("+----------------------------------------------------------------------------------------------------------------------------------------+\n");
+            
+        stock += buku[i].Jumlah_tersedia;
+
+        if(i == ((page + 1) * data_per_page) - 1 || i == data - 1){
+            printf("\nJumlah buku \t : %d\n", data);
+            printf("Jumlah stock buku : %d\n", stock);
+            printf("------------------------------------------------------------------------------------------------------------------------------------------\n");
+            printf("------------------------------------------------------------------------------------------------------------------------------------------\n\n");
+            break;
+        }
+    }
+}
+
 void List_Buku(){
-    char buffer[512];
-    Perpus buku[20];
+    char buffer[256];
+    int pilihan;
+    int page = 0;
+    Perpus buku[Max_char];
     FILE * fp;
     fp = fopen("Data_Buku.txt", "r");
     if(fp == NULL){
-        printf("File Error while reading...");
+        printf("Error while reading...");
     }
-    
+
     int data = 0;
-    while(fgets(buffer, 512, fp) != NULL){
-            sscanf(buffer, "%u, \"%[^\"]\", \"%[^\"]\", \"%[^\"]\", %u, %u, %u", &buku[data].Id_buku, buku[data].Judul, buku[data].Penulis,
-                   buku[data].Penerbit, &buku[data].Jumlah_halaman, &buku[data].Tahun_terbit, &buku[data].Jumlah_tersedia);
-            data++;
-        }
-    
+    while(fgets(buffer, 256, fp) != NULL){
+        sscanf(buffer, "%u, \"%[^\"]\", \"%[^\"]\", \"%[^\"]\", %u, %u, %u", &buku[data].Id_buku, buku[data].Judul, buku[data].Penulis,
+               buku[data].Penerbit, &buku[data].Jumlah_halaman, &buku[data].Tahun_terbit, &buku[data].Jumlah_tersedia);
+        data++;
+    }
+
     fclose(fp);
+    int data_per_page = 10;
+    int total_page = (data + data_per_page) / data_per_page;
+
+    Display_List_Buku(page, data_per_page, data, buku);
+
+    do{
+        printf("Pilihan Opsi : ");
+        printf("1. Page selanjutnya \n2. Page sebelumnya \n3. Kembali");
+        printf("Pilihan anda (1-3) : %d", pilihan);
+        scanf("%d", &pilihan);
+
+        switch (pilihan)
+        {
+        case 1 :
+            if(page < total_page - 1){
+                page++;
+                Clear_System();
+                Display_List_Buku(page, data_per_page, data, buku);
+            } else{
+                Clear_System();
+                Display_List_Buku(page, data_per_page, data, buku);
+                printf("\nAnda sudah berada di page terakhir.\n\n");
+            }
+            break;
+
+        case 2 :
+            if(page > 0){
+                page--;
+                Clear_System();
+                Display_List_Buku(page, data_per_page, data, buku);
+            } else{
+                Clear_System();
+                Display_List_Buku(page, data_per_page, data, buku);
+                printf("\nAnda sudah berada di page pertama.\n\n");
+            }
+            break;
+
+        case 3 :
+            break;
         
-        for(int i = 0; i < 20; i++){
-            if(i == 0){
-                printf("+----------------------------------------------------------------------------------------------------------------------------------------+\n");
-                printf("| Id_buku | \t\t Judul Buku \t\t | \t Penulis \t | \t Penerbit \t | Jumlah_halaman | Tahun_terbit | stock |\n");
-                printf("+----------------------------------------------------------------------------------------------------------------------------------------+\n");
-            }
-            printf("| %-7u | %-36s | %-21s | %-21s | %-14u | %-12u | %-5u |\n", buku[i].Id_buku, buku[i].Judul, 
-                   buku[i].Penulis, buku[i].Penerbit, buku[i].Jumlah_halaman, buku[i].Tahun_terbit, buku[i].Jumlah_tersedia);
-            printf("+----------------------------------------------------------------------------------------------------------------------------------------+\n");
-            if(i == data - 1){
-                break;
-            }
+        default:
+            Clear_System();
+            Display_List_Buku(page, data_per_page, data, buku);
+            printf("Pilihan anda tidak valid. Silahkan ulangi (1-3).\n\n");
+            break;
         }
-        
+
+    } while(pilihan != 3);
+
     printf("\n\n");
 }
